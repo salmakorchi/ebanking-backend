fd96942e2d2d6aa05529d59b410683fa734559b6
salmakorchi <57690392+salmakorchi@users.noreply.github.com>
2022-06-23 19:07:43 +0100

    V2- part 33:28

--- src/main/java/com/example/ebankingbackend/services/BankAccountService.java
+++ src/main/java/com/example/ebankingbackend/services/BankAccountService.java
@@ -2,13 +2,14 @@ package com.example.ebankingbackend.services;
 
 import com.example.ebankingbackend.entities.BankAccount;
 import com.example.ebankingbackend.entities.Customer;
+import com.example.ebankingbackend.exceptions.CustomerNotFoundException;
 
 import java.util.List;
 
 public interface BankAccountService {
 
     Customer saveCustomer(Customer customer);
-    BankAccount saveBankAccount(double initialBalance,String type,Long customerId);
+    BankAccount saveBankAccount(double initialBalance,String type,Long customerId) throws CustomerNotFoundException;
     List<Customer> listCustomer();
     BankAccount getBankAccount(String accountId);
     void debit(String accountId,double amount,String description);
--- src/main/java/com/example/ebankingbackend/services/BankAccountServiceImpl.java
+++ src/main/java/com/example/ebankingbackend/services/BankAccountServiceImpl.java
@@ -1,20 +1,34 @@
 package com.example.ebankingbackend.services;
 
 import com.example.ebankingbackend.entities.BankAccount;
+import com.example.ebankingbackend.entities.CurrentAccount;
 import com.example.ebankingbackend.entities.Customer;
+import com.example.ebankingbackend.entities.SavingAccount;
+import com.example.ebankingbackend.exceptions.CustomerNotFoundException;
 import com.example.ebankingbackend.repositories.AccountOperationRepository;
 import com.example.ebankingbackend.repositories.BankAccountRepository;
 import com.example.ebankingbackend.repositories.CustomerRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional // make sure you're using the spring transactional method not javax
+
 //instead of Autowired we use :
 @AllArgsConstructor
+
+// instead of Logger log = LoggerFactory.getLogger(this.getClass().getName());
+//we can use lombok annotation :
+@Slf4j
+
 public class BankAccountServiceImpl implements  BankAccountService{
 
   //  @Autowired // DEPENDENCIES INJECTION (best to use constructors instead )
@@ -31,13 +45,36 @@ public class BankAccountServiceImpl implements  BankAccountService{
         this.accountOperationRepository = accountOperationRepository;
     }*/
 
+
+
     @Override
     public Customer saveCustomer(Customer customer) {
-        return null;
+        log.info("Saving a new Customer");
+        Customer savedCustomer= customerRepository.save(customer);
+
+        return savedCustomer;
     }
 
     @Override
-    public BankAccount saveBankAccount(double initialBalance, String type, Long customerId) {
+    public BankAccount saveBankAccount(double initialBalance, String type, Long customerId) throws CustomerNotFoundException {
+       BankAccount bankAccount;
+       Customer customer = customerRepository.findById(customerId).orElse(null);
+        if (customer==null){
+            throw new CustomerNotFoundException("Customer not found");
+
+        }
+       if(type.equals("current")){
+           bankAccount= new CurrentAccount();
+
+       }else{
+           bankAccount= new SavingAccount();
+        }
+       bankAccount.setId(UUID.randomUUID().toString());
+       bankAccount.setCreatedAt(new Date());
+       bankAccount.setBalance(initialBalance);
+       bankAccount.setCustomer(customer);
+       bankAccount.
+
         return null;
     }
 
