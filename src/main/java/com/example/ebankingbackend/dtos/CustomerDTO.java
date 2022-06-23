374c811c09086210783b9c10212f361c029dae1d
salmakorchi <57690392+salmakorchi@users.noreply.github.com>
2022-06-23 22:31:03 +0100

    V2- part2 1:30:20 DTOS and mappers

---
+++ src/main/java/com/example/ebankingbackend/dtos/CustomerDTO.java
@@ -0,0 +1,13 @@
+package com.example.ebankingbackend.dtos;
+
+
+import lombok.Data;
+
+
+
+@Data
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
